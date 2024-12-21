a9a8fee5746681389793cc0f76fbe91ebf125a59
kannan-ravi <kannanravindran00kr@gmail.com>
2024-12-21 23:15:01 +0530

    Implemented Inifnite scrolling

--- src/pages/feeds/Feeds.jsx
+++ src/pages/feeds/Feeds.jsx
@@ -16,18 +16,20 @@ import { db } from "../../../firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllPost } from "../../app/features/postSlice";
 import { Client } from "appwrite";
+import { toastError } from "../../app/features/toastSlice";
 
 const Feeds = () => {
   const client = new Client();
   client.setProject("vibesnap");
 
-  const [items, setItems] = useState([]);
+  const dispatch = useDispatch();
+  const { posts } = useSelector((state) => state.post);
+
   const [lastDoc, setLastDoc] = useState(null);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
 
-  const dispatch = useDispatch();
-  const { posts } = useSelector((state) => state.post);
+  const LIMIT = 1;
 
   const getUserData = async (userRef) => {
     if (!userRef) return null;
@@ -36,28 +38,55 @@ const Feeds = () => {
   };
 
   const fetchPosts = async () => {
+    if (loading || !hasMore) return;
+
+    setLoading(true);
+
     try {
-      setLoading(true);
       const postRef = collection(db, "posts");
       const postsQuery = lastDoc
-        ? query(postRef, orderBy("created_at"), startAfter(lastDoc), limit(20))
-        : query(postRef, orderBy("created_at"), limit(20));
+        ? query(
+            postRef,
+            orderBy("created_at"),
+            startAfter(lastDoc),
+            limit(LIMIT)
+          )
+        : query(postRef, orderBy("created_at"), limit(LIMIT));
 
       const snapShot = await getDocs(postsQuery);
+      if (!snapShot.empty) {
+        const newPosts = await Promise.all(
+          snapShot.docs.map(async (doc) => {
+            const postData = { id: doc.id, ...doc.data() };
+            const userData = await getUserData(postData.posted_by);
+            return {
+              ...postData,
+              posted_by: userData,
+            };
+          })
+        );
 
-      const newItems = await Promise.all(
-        snapShot.docs.map(async (doc) => {
-          const postData = { id: doc.id, ...doc.data() };
-          const userData = await getUserData(postData.posted_by);
-          return {
-            ...postData,
-            posted_by: userData,
-          };
-        })
-      );
-      dispatch(fetchAllPost(newItems.reverse()));
+        dispatch(
+          fetchAllPost([
+            ...(posts || []),
+            ...newPosts.filter(
+              (newPost) => !posts.some((post) => post.id === newPost.id)
+            ),
+          ])
+        );
+        setLastDoc(snapShot.docs[snapShot.docs.length - 1]);
+
+        if (snapShot.docs.length < LIMIT) {
+          setHasMore(false);
+        }
+      } else {
+        setHasMore(false);
+      }
     } catch (error) {
       console.log(error);
+      dispatch(toastError("Failed to fetch posts"));
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -95,6 +124,15 @@ const Feeds = () => {
           </div>
         )}
       </div>
+
+      {loading && (
+        <div className="flex items-center justify-center my-6">
+          <div className="w-10 h-10 border-2 border-t-2 border-black animate-spin"></div>
+        </div>
+      )}
+      {!hasMore && !loading && (
+        <p className="mt-6 text-center text-gray-500">No more posts to load.</p>
+      )}
 
       <Link
         to="/create-post"
